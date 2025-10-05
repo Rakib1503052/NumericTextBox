bd378dff8b4bef0a5de634464da1ae542bd19320
Rakib1503052 <51320424+Rakib1503052@users.noreply.github.com>
2025-10-05 19:55:56 +0900

    Too long XML URI

--- NumericTextBox/AssemblyInfo.cs
+++ NumericTextBox/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Windows.Markup;
 
 [assembly: ComVisible(false)]
 
-[assembly: XmlnsDefinition("https://github.com/Rakib1503052/NumericTextBox/blob/master/NumericTextBox/README", "NumericTextBox")]
-[assembly: XmlnsPrefix("https://github.com/Rakib1503052/NumericTextBox/blob/master/NumericTextBox/README", "ntb")]
+[assembly: XmlnsDefinition("https://github.com/Rakib1503052/NumericTextBox/", "NumericTextBox")]
+[assembly: XmlnsPrefix("https://github.com/Rakib1503052/NumericTextBox/", "ntb")]
