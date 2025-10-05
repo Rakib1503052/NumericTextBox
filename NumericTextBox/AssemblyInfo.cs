86770a890421558e0639fdffb45d6f7f7714f21a
Rakib1503052 <51320424+Rakib1503052@users.noreply.github.com>
2025-10-05 19:21:48 +0900

    Added readme and XML URI

---
+++ NumericTextBox/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: ComVisible(false)]
+
+[assembly: XmlnsDefinition("https://github.com/Rakib1503052/NumericTextBox/blob/master/NumericTextBox/README", "NumericTextBox")]
+[assembly: XmlnsPrefix("https://github.com/Rakib1503052/NumericTextBox/blob/master/NumericTextBox/README", "ntb")]
